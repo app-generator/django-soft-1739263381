c723a45cdb76e736f2b706b2ea9d45315333178d
App Generator <51070104+app-generator@users.noreply.github.com>
2025-02-11 10:43:10 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Lights(models.Model):
+
+    #__Lights_FIELDS__
+    temp = models.IntegerField(null=True, blank=True)
+    voltage = models.IntegerField(null=True, blank=True)
+    current = models.IntegerField(null=True, blank=True)
+
+    #__Lights_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Lights")
+        verbose_name_plural = _("Lights")
+
+
+
+#__MODELS__END
